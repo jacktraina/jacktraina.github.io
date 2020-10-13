8547bfa912b11648ed47e81a5419fde9c77eceaa
Jack Traina <jrt543@gmail.com>
2020-10-13 11:38:54 -0700

    first commit

---
+++ build/precache-manifest.2abc2f1059b4734698340ba918388127.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb7494ee5b0ff2450917be323010ce6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75adfb30e69eef9e595d",
+    "url": "/static/css/main.b5cb1115.chunk.css"
+  },
+  {
+    "revision": "d1b2fc21ddfa3243fca0",
+    "url": "/static/js/2.7f8d49ff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.7f8d49ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75adfb30e69eef9e595d",
+    "url": "/static/js/main.fffd14c5.chunk.js"
+  },
+  {
+    "revision": "bd08ba29cf3645b6cbb5",
+    "url": "/static/js/runtime-main.05f7ce75.js"
+  },
+  {
+    "revision": "6b68201e3e13a373c8f4eb0a03444e21",
+    "url": "/static/media/adobe-illustrator.6b68201e.png"
+  },
+  {
+    "revision": "2c93fb1ff0ed54b679b8ea346a7777be",
+    "url": "/static/media/bootstrap.2c93fb1f.png"
+  },
+  {
+    "revision": "7826c363005078d1e36cb4513a5af707",
+    "url": "/static/media/charity-app.7826c363.png"
+  },
+  {
+    "revision": "86d98420761575936bd8f4b234c5fb48",
+    "url": "/static/media/creative-tim.86d98420.png"
+  },
+  {
+    "revision": "95728e673688e884535023a87c67d3a6",
+    "url": "/static/media/css.95728e67.png"
+  },
+  {
+    "revision": "e0b237e8b205133d689761dfe922e6cd",
+    "url": "/static/media/dots.e0b237e8.png"
+  },
+  {
+    "revision": "412939075239c63dd0a6703a1b90d932",
+    "url": "/static/media/fullpage.41293907.png"
+  },
+  {
+    "revision": "80d6fdeed0221624cbd3d461ff9373a2",
+    "url": "/static/media/giving-tree.80d6fdee.png"
+  },
+  {
+    "revision": "572576b5fb0f37daad60da313558f4b1",
+    "url": "/static/media/giving-tree1.572576b5.png"
+  },
+  {
+    "revision": "a8543f73e650789085f8f7b16ae8c4da",
+    "url": "/static/media/giving-tree2.a8543f73.png"
+  },
+  {
+    "revision": "561ff81b1993800a8eb4f198f19abc4b",
+    "url": "/static/media/giving-tree3.561ff81b.png"
+  },
+  {
+    "revision": "1d0ca22b28f22ceb9d72d7c422a19e25",
+    "url": "/static/media/iphone-darwin.1d0ca22b.png"
+  },
+  {
+    "revision": "db63e7e2e77eb5abb1dea40bb201cd3d",
+    "url": "/static/media/iphone-touro.db63e7e2.png"
+  },
+  {
+    "revision": "9bb07247d0c001319155c42b8cde7829",
+    "url": "/static/media/me.9bb07247.jpg"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "e62d30e8e78373f1b42b50aae990f63c",
+    "url": "/static/media/path3.e62d30e8.png"
+  },
+  {
+    "revision": "417e72a92435c348d8953c419bb9464d",
+    "url": "/static/media/react-portfolio.417e72a9.png"
+  },
+  {
+    "revision": "49a30f9528092997b65a0d1c998e16a6",
+    "url": "/static/media/react.49a30f95.png"
+  },
+  {
+    "revision": "5d370eaf3004a832d83b2294c28f4165",
+    "url": "/static/media/square1.5d370eaf.png"
+  },
+  {
+    "revision": "5b1e54e0390a39ac23e39138e5702682",
+    "url": "/static/media/square2.5b1e54e0.png"
+  },
+  {
+    "revision": "ef4e0a705ca9b44a413e24cc2d4f4a41",
+    "url": "/static/media/square3.ef4e0a70.png"
+  },
+  {
+    "revision": "eb5c914326e0b14b7b9d0994124d6eca",
+    "url": "/static/media/square4.eb5c9143.png"
+  },
+  {
+    "revision": "fc998805d72848ccda24724342bb32f6",
+    "url": "/static/media/square5.fc998805.png"
+  },
+  {
+    "revision": "40502acf50633de51e89bb32fc220801",
+    "url": "/static/media/tour-home.40502acf.png"
+  },
+  {
+    "revision": "e98bb83188a404102fa3ab714a58080d",
+    "url": "/static/media/tour-site.e98bb831.png"
+  },
+  {
+    "revision": "1bce8091dc7cacfbe8d181c735b2164d",
+    "url": "/static/media/touro.1bce8091.png"
+  }
+]);
